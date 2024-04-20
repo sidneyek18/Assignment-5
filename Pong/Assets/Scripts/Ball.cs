02bf3200af65d84909d85602477c71303052f6f7
Sidney <im.sydney.kiss@gmail.com>
2024-04-19 21:03:08 -0400

    Update UI When Ball Score

--- Pong/Assets/Scripts/Ball.cs
+++ Pong/Assets/Scripts/Ball.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.U2D;
 
 public class Ball : MonoBehaviour
 {
+    public TMP_Text playerScoreText;
+    public TMP_Text AIScoreText;
+
+    int playerScore = 0;
+    int AIScore = 0;
+
     //Ball speed
     float speed = 5f;
     Rigidbody2D rigidbody;
@@ -19,5 +27,29 @@ public class Ball : MonoBehaviour
     void OnCollisionEnter2D(Collision2D collision)
     {
         rigidbody.AddForce(transform.up * -speed, ForceMode2D.Impulse);
+    }
+    
+    //When the ball hits the green square increase the score and respawn the ball in the middle.
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        //If the ball goes in the green rectangle behind the AI paddle.
+        if (collision.name == "PlayerScore")
+        {
+            playerScore++;
+            playerScoreText.text = playerScore.ToString();
+            Debug.Log("Player Score");
+        }
+        //If the ball goes in the green rectangle behind the player paddle.
+        if (collision.name == "AIScore")
+        {
+            AIScore++;
+            Debug.Log("AI Score");
+            AIScoreText.text = playerScore.ToString();
+        }
+
+        //Reset ball in the middle of the screen.
+        transform.position = Vector3.zero;
+        rigidbody.AddForce(transform.up * speed, ForceMode2D.Impulse);
+
     }
 }
